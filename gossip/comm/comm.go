3dd425fa2ff99d5e173b6bfa66505c504fb41032
docbull <fudghks21@gmail.com>
2022-04-20 03:05:36 +0900

    GossipSub block sender function in Comm interface

--- gossip/comm/comm.go
+++ gossip/comm/comm.go
@@ -26,7 +26,8 @@ type Comm interface {
 	// Send sends a message to remote peers asynchronously
 	Send(msg *protoext.SignedGossipMessage, peers ...*RemotePeer)
 
-	// SendGossipSub sends a block data to remote peers using libp2p GossipSub
+	// SendGossipSub sends a block data to remote peers using libp2p GossipSub,
+	// and this function will be called for block dissemination only
 	SendGossipSub(msg *protoext.SignedGossipMessage, peers ...*RemotePeer)
 
 	// SendWithAck sends a message to remote peers, waiting for acknowledgement from minAck of them, or until a certain timeout expires
